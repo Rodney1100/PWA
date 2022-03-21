e2928d0798c007d98685c39a094d4ee07e25cd99
Rodney P <rodney.plancher@gmail.com>
2022-03-21 16:46:28 -0400

    webpack issue

--- assets/js/domMethods.js
+++ assets/js/domMethods.js
@@ -1,5 +1,3 @@
-// require("bootstrap");
-// const createEl = require("./domMethods");
 function createEl(htmlString, attrs, ...children) {
   if (typeof htmlString !== "string") {
     throw Error("Argument 'htmlString' is required and must be a string");
@@ -11,13 +9,17 @@ function createEl(htmlString, attrs, ...children) {
     for (let key in attrs) {
       if (key.substring(0, 2) === "on") {
         el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
+      } else if (key === "style") {
+        for (let rule in attrs[key]) {
+          el.style[rule] = attrs[key][rule];
+        }
       } else {
         el.setAttribute(key, attrs[key]);
       }
     }
   }
 
-  children.forEach(function (child) {
+  children.forEach(function(child) {
     let node;
 
     if (child.constructor.name.includes("Element")) {
